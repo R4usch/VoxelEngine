ec59e1bf722e335171917be71129cb51b105396b
R4usch <worldofrausch@gmail.com>
2024-02-01 18:35:34 -0300

    Drawing a quad

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,20 +15,15 @@ class Program
     static void Main(string[] args)
     {
 
-        float[] vertices = 
+        float[] vertices =
         [
-                
-                0.5f, 0.5f, 0.0f,
-                -0.5f, -0.5f, 0.0f,
-                0.5f, -0.5f, 0.0f
-        ];
+            0.5f, 0.5f, 0f,     // Triangle 1
+            0.5f, -0.5f, 0f,
+            -0.5f, -0.5f, 0.0f, // Triangle 1
 
-        float[] vertices2 = 
-        [
-                
-                -0.5f, 0.5f, 0.0f,
-                -0.5f, -0.5f, 0.0f,
-                0.5f, -0.5f, 0.0f
+            -0.5f, 0.5f, 0.0f,  // Triangle 2
+            0.5f, 0.5f, 0.0f,
+            -0.5f,-0.5f,0.0f    // Triangle 2
         ];
 
         var gameWindow = new GameWindow(new GameWindowSettings(), new NativeWindowSettings
@@ -47,20 +42,22 @@ class Program
 
         gameWindow.Load += () =>
         {
-
-            // Bind vao (VERTEX ARRAY OBJECT)
+            GL.Enable(EnableCap.DepthTest);
             GL.ClearColor(0.0f, 0.15f, 0.25f, 1.0f); // Cor de fundo
+
+            // VAO = Um objeto que armazena os ponteiros das vertices que estão no VBO ( eu acho )
+            // Bind vao (VERTEX ARRAY OBJECT)
             VAO = GL.GenVertexArray();
             GL.BindVertexArray(VAO);
 
-
+            // VBO = Local na memoria onde sera armazenado as vertices
             // Bind VBO (VERTEX BUFFER OBJECT)
             VBO = GL.GenBuffer();
             GL.BindBuffer(BufferTarget.ArrayBuffer, VBO);
             GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices.ToArray(), BufferUsageHint.StaticDraw);
 
             GL.EnableVertexAttribArray(0);
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, true, 0,0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, true, 0, 0);
         };
 
         gameWindow.RenderFrame += (sender) =>
@@ -73,14 +70,16 @@ class Program
 
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+            GL.BindVertexArray(VAO);
             // Preparo do viewport
 
 
             // Renderiza a partir daqui
 
-            GL.BindVertexArray(VAO);
 
-            GL.DrawArrays(PrimitiveType.Triangles, 0, vertices.Length);
+            // 0 = Onde vai começar a ler as vertices
+            // 3 = E a quantidade de vertices
+            GL.DrawArrays(PrimitiveType.Triangles, 0, vertices.Length / 3);  // 2 triangles * 3 vertices
             
 
             gameWindow.SwapBuffers();
